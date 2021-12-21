2ef8b83c4050095c3318eb12af8d080e620b39a0
Carolina Munoz <carito.camo@gmail.com>
2021-12-20 22:33:22 -0300

    Se reorganizan carpetas del proyecto

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../resources/images/logoDevsUnited.svg";
+import logoGoogle from "../resources/images/logoGoogle.svg";
+
+function WelcomePage() {
+    return (
+
+    )
+}
+
+export default WelcomePage;
