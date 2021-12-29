744401d1f1b1fe8603ccb6cc0d242a76d5607b18
Carolina Munoz <carito.camo@gmail.com>
2021-12-29 01:25:15 -0300

    Se crea nuevo page para user profile B

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import UserProfile from './pages/UserProfile';
+import UserProfileB from './pages/UserProfileB';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <UserProfile />
+      <UserProfileB />
     </div>
   );
 }
---
+++ src/pages/UserProfileB.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import back from "../resources/images/back.svg";
+import logout from "../resources/images/logout.svg";
+import profilePicture from "../resources/images/profilePicture.svg";
+import TweetContainer from "../components/TweetContainer";
+
+function UserProfileB() {
+    return (
+        <section className="userProfile">
+            <header className="headerUserProfile">
+                <div className="returnBox">
+                    <img src={back} alt="Boton volver atrás" className="buttonBack"/>
+                    <h1 className="username">Username</h1>
+                </div>
+            </header>
+            <article className="userInformation">
+                <img src={profilePicture} alt="Foto de perfil" className="profilePicture"/>
+                <h1 className="username">Username</h1>
+            </article>
+            <TweetContainer />
+            <TweetContainer />
+            <TweetContainer />
+        </section>
+    )
+}
+
+export default UserProfileB;
