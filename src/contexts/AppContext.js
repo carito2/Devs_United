71bb941a90f37679e3a8d73f02c87eaa24ea4a46
Carolina Munoz <carito.camo@gmail.com>
2022-01-14 23:48:33 -0300

    Se mueve llamada a firebase a contexto

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,4 +1,5 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
+import {firestore, auth} from "../firebase/firebase";
 
 export const AppContext = createContext();
 
@@ -15,6 +16,44 @@ export const AppProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [userProfile, setUserProfile] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if(user) {
+            setLoading(true);
+            const unsubscribe = firestore
+                .collection("tweets")
+                .onSnapshot((snapshot) => {
+                    const tweets = snapshot.docs.map((doc) => {
+                    return {
+                        profilePicture: doc.data().profilePicture,
+                        tweet: doc.data().tweet,
+                        username: doc.data().autor,
+                        email: doc.data().email,
+                        likes: doc.data().likes,
+                        numLike: doc.data().numLike,
+                        date: doc.data().date,
+                        uid: doc.data().uid,
+                        id: doc.id
+                    }
+                    })
+                    setTweets(tweets);
+                    setLoading(false);
+                });
+                return () => unsubscribe();
+            }
+        auth.onAuthStateChanged((user) => {
+            setUser(user);
+            firestore
+            .collection("usersProfile").where("uid", "==", user.uid)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    setUserProfile(doc.data());
+                })
+            })
+        });
+    },[user]);
+
     return (
         <AppContext.Provider value={{
             tweet, 
